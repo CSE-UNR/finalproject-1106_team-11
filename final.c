8bb91c3aa73ac060598c92610724969c4a3640fb
1106-Alejandro-Kace <kalejandro@nevada.unr.edu>
2024-05-05 07:30:50 +0000

    Kaces part of project

--- final.c
+++ final.c
@@ -1,24 +1,64 @@
-//Names
-//Date
-//Class
-//Purpose
+//Names: Kace Alejandro,
+//Date:
+//Class: CS 135.1106
+//Purpose: Image reading group project
 
-//Add loadimage()
-//Add displayimage()
 //Add editimage()
 //cropimage()
 //dimimage()
 //brightenimage()
 
-//Kace (load image and display image)
-int loadimage(double
+#include <stdio.h>
 
-//Reese (editimage and cropimage)
+// Kace
+// Function to load a new image from a file
+void loadImage() {
+	// Implementation goes here
+	printf("Loading image...\n");
+}
 
-//Althea (dimimage and brightenimage)
+// Kace
+// Function to display the current image
+void displayImage() {
+	// Implementation goes here
+	printf("Displaying the current image...\n");
+}
 
+// Reese
+
+// Althea
 
 int main() {
+	int choice;
+	
+	do {
 
+		// Display the menu options
+		printf("Menu Options:\n");
+		printf("1. Load a new image\n");
+		printf("2. Display the current image\n");
+		printf("3. Edit the current image\n");
+		printf("4. Exit the program\n");
+		printf("Enter your choice: ");
+		scanf("%d", &choice);
 
-return 0;
+	switch (choice) {
+	case 1:
+		loadImage();
+		break;
+	case 2:
+		displayImage();
+		break;
+	case 3:
+		editImage();
+		break;
+	case 4:
+		saveImage();
+		break;
+	default:
+		printf("Invalid choice. Please try again.\n");
+		}
+	} while (choice != 4);
+
+    return 0;
+}
